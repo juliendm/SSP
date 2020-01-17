58b8a959e409ff4a34a77ee21564d7aad01599af
juliendm <juliendemuelenaere@Juliens-MacBook-Pro-3.local>
2020-01-17 12:32:07 -0800

    Updates

--- yolov3/detect.py
+++ yolov3/detect.py
@@ -13,7 +13,7 @@ import pickle
 import cv2, json
 from scipy.spatial.transform import Rotation
 
-from scipy.optimize import fmin_bfgs
+from scipy.optimize import minimize
 
 import json
 
@@ -47,6 +47,11 @@ def detect(cfgfile, weightfile, imgfile):
 
     submission = {}
     
+    K = np.array([[2304.5479, 0,  1686.2379],
+                  [0, 2305.8757, 1354.9849],
+                  [0, 0, 1]], dtype=np.float32)
+    num_keypoints = 10
+
     visualize = True
 
     for index,imgfile in enumerate(imgfiles):
@@ -72,11 +77,8 @@ def detect(cfgfile, weightfile, imgfile):
             pass
             # img.save(imgpath_preprocessed)
 
-        # with open(segm_path,'rb') as pkl_file:
-        #     segms = pickle.load(pkl_file)
-
-        # with open(yolov3_path,'rb') as pkl_file:
-        #     boxes_yolov3 = pickle.load(pkl_file)
+        with open(segm_path,'rb') as pkl_file:
+            segms = pickle.load(pkl_file)
 
         if (img_id in flipped) and flipped[img_id]:
             img = img.transpose(Image.FLIP_LEFT_RIGHT)
@@ -86,56 +88,19 @@ def detect(cfgfile, weightfile, imgfile):
         # NN Predictions
 
         start = time.time()
-        boxes = do_detect(m, sized, 0.5, 0.3, use_cuda)
+        boxes = do_detect(m, sized, 0.6, 0.3, use_cuda)
         correct_yolo_boxes(boxes, img.width, img.height, m.width, m.height)
         finish = time.time()
         print('%s: Predicted in %f seconds.' % (imgfile, (finish-start)))
 
-
-        # IOU boxes, boxes_yolov3
-
-        boxes = np.array(boxes)
-        boxes_mask = np.ones(boxes.shape[0], dtype=bool)
-        filtered_boxes = []
-
-        # print(len(boxes_yolov3))
-
-        # for k in range(len(boxes_yolov3)):
-        #     gt_boxes = torch.FloatTensor(boxes_yolov3[k])
-        #     gt_boxes = gt_boxes.repeat(len(boxes[boxes_mask]),1).t()
-        #     pred_boxes = torch.FloatTensor(boxes[boxes_mask]).t()
-        #     best_iou, best_i = torch.max(multi_bbox_ious(gt_boxes, pred_boxes, x1y1x2y2=False),0)
-        #     if best_iou > 0.1:
-        #         filtered_boxes.append(boxes[boxes_mask][best_i])
-        #     boxes_mask[best_i] = 0
-        # filtered_boxes = np.array(filtered_boxes)
-
-        # print(len(filtered_boxes))
-
-        filtered_boxes = boxes
-
-
-            # # pred_boxes and gt_boxes are transposed for torch.max
-            # if best_iou > iou_thresh and pred_boxes[6][best_j] == gt_boxes[6][0]:
-            #     correct += 1
-
-
-        # ious = multi_bbox_ious( torch.FloatTensor(boxes).t(),  torch.FloatTensor(boxes_yolov3).t(), x1y1x2y2=False)
-
-        # print(ious)
-
-        # Process Predictions
-
-        vertices_2D = []
-        vertices_2D_colored = []
-        triangles_2D = []
+        # ------------
+        #  Loop Boxes
+        # ------------
 
         pred_candidates = []
 
-        num_keypoints = 10
-
-        for i in range(len(filtered_boxes)):
-            box = filtered_boxes[i]
+        for i in range(len(boxes)):
+            box = boxes[i]
 
             if (img_id in flipped) and flipped[img_id]:
                 box[0] =  1.0 - box[0]
@@ -164,15 +129,9 @@ def detect(cfgfile, weightfile, imgfile):
 
             with open('../../baidu_data/models/json/%s.json' % car_id2name[model_id]) as json_file:
                 data = json.load(json_file)
-
             vertices  = np.c_[np.array(data['vertices']), np.ones((len(data['vertices']), 1))].transpose()
-            triangles = np.array(data['faces'])-1
-            corners3D     = get_3D_corners(vertices, mean=True)   
+            corners3D     = get_3D_corners(vertices)
             objpoints3D = np.array(np.transpose(np.concatenate((np.zeros((3, 1)), corners3D[:3, :]), axis=1)), dtype='float32')
-
-            K = np.array([[2304.5479, 0,  1686.2379],
-                          [0, 2305.8757, 1354.9849],
-                          [0, 0, 1]], dtype=np.float32)
 
             # Denormalize the corner predictions 
             corners2D = np.array(box[7:7+2*(num_keypoints-1)], dtype='float32').reshape(num_keypoints-1,2)
@@ -182,104 +141,149 @@ def detect(cfgfile, weightfile, imgfile):
             R_pr, t_pr = pnp(objpoints3D,  corners2D, K)
             Rt_pr      = np.concatenate((R_pr, t_pr), axis=1)
 
-
-            # # Make Prediction Candidates
-            # angles = Rotation.from_dcm(R_pr.T).as_euler('xyz')
-            # pred_candidate = [angles[0],angles[1],-angles[2]+np.pi,t_pr[0,0],t_pr[1,0],t_pr[2,0],box[5],model_id]
-            
             # Make Prediction Candidates
-            angles = Rotation.from_dcm(R_pr.T).as_euler('yxz')
-            angle_y = angles[0]
-            angle_z = angles[2]+np.pi
-            angle_x = angles[1]*np.sign(rotate(angle_y,np.pi/2.0))
-            if angle_z > np.pi: angle_z -= 2.0*np.pi
-            pred_candidate = [angle_x,angle_y,angle_z,t_pr[0,0],t_pr[1,0],t_pr[2,0],box[5],model_id]
+            angles = Rotation.from_dcm(R_pr.T).as_euler('xyz')
+            pred_candidate = [angles[0],angles[1],-angles[2]+np.pi,t_pr[0,0],t_pr[1,0],t_pr[2,0],box[5],model_id]
+            
+            # # Make Prediction Candidates
+            # angles = Rotation.from_dcm(R_pr.T).as_euler('yxz')
+            # angle_y = angles[0]
+            # angle_z = angles[2]+np.pi
+            # angle_x = angles[1]*np.sign(rotate(angle_y,np.pi/2.0))
+            # if angle_z > np.pi: angle_z -= 2.0*np.pi
+            # pred_candidate = [angle_x,angle_y,angle_z,t_pr[0,0],t_pr[1,0],t_pr[2,0],box[5],model_id]
 
             pred_candidates.append(pred_candidate)
             
-            x0 = [t_pr[0,0],t_pr[1,0],t_pr[2,0],angle_x,angle_y,angle_z]
 
-            # # Optim:
-            # res = fmin_bfgs(neg_iou_mask, x0, args=(coords,mask),epsilon=1e-03,disp=1)
-            # #x_sol = [-5.00803688 ,2.9252357 ,12.67238289 ,0.15001176 ,-0.0128817 , -0.04438048]
+        # ------------
+        #  Loop Masks
+        # ------------
 
-            # iou = -neg_iou_mask(x0,coords,mask,save=True)
-            # print(iou)
+        pred_checked = []
 
-            # R_pr = Rotation.from_euler('xyz', x_sol[3:]).as_dcm().T
+        pred_candidates = np.array(pred_candidates)
+        preds_mask = np.ones(pred_candidates.shape[0], dtype=bool)
 
-            x_sol = x0
+        for segm_idx in range(len(segms)):
 
-            R_pr = Rotation.from_euler('xyz', [x_sol[3], x_sol[4], -x_sol[5]+np.pi]).as_dcm().T
-            Rt_pr = np.concatenate((R_pr, np.array(x_sol[:3]).reshape(-1,1)), axis=1)
+            match_idx = 0
+            max_iou = 0.0
 
-        
+            for pred_idx in range(len(pred_candidates[preds_mask])):
+                pred = pred_candidates[preds_mask][pred_idx]
+                x0 = [pred[3],pred[4],pred[5],pred[0],pred[1],pred[2]]
+                model_id = pred[7]
+                with open('../../baidu_data/models/pkl/%s.pkl' % car_id2name[model_id], 'rb') as pkl_file:
+                    coords = pickle.load(pkl_file)
+                coords[(coords[:,3]==0)&(coords[:,1]<-0.3),3]=7
+
+                iou = -neg_iou_mask(x0,coords,segms[segm_idx])
+                if iou > max_iou:
+                    max_iou = iou
+                    match_idx = pred_idx
+            if max_iou == 0.0:
+                continue
+
+            print(max_iou)
+            pred = pred_candidates[preds_mask][match_idx]
+
+            print(pred)
+
+
+
+            optim = False
+            if optim:
+
+                raise NotImplementedError
+                # Optional Optim
+
+                # x0 = [t_pr[0,0],t_pr[1,0],t_pr[2,0],angle_x,angle_y,angle_z]
+                # bounds = ((x0[0]-3.0, x0[0]+3.0), (x0[1]-3.0, x0[1]+3.0), (x0[2]-3.0, x0[2]+3.0), (-0.3, 0.3), (x0[4]-0.25*np.pi, x0[4]+0.25*np.pi), (x0[5]-0.1*np.pi, x0[5]+0.1*np.pi))
+
+                # # # Optim:
+                # # res = fmin_bfgs(neg_iou_mask, x0, args=(coords,mask),epsilon=1e-03,disp=1)
+                # # #x_sol = [-5.00803688 ,2.9252357 ,12.67238289 ,0.15001176 ,-0.0128817 , -0.04438048]
+
+                # x_sol = minimize(neg_iou_mask, x0, args=(coords,mask), method='SLSQP', bounds=bounds, tol=None, options={'maxiter': 100, 'ftol': 1e-06, 'iprint': 1, 'disp': False, 'eps': 1e-03})
+                
+                # print(x_sol)
+
+                # # iou = -neg_iou_mask(x0,coords,mask,save=True)
+                # # print(iou)
+
+                # # R_pr = Rotation.from_euler('xyz', x_sol[3:]).as_dcm().T
+
+                # # x_sol = x0
+
+
+            pred_checked.append(pred)
             
-            if visualize:
+            preds_mask[pred_idx] = 0
+
+                
+        if visualize:
+
+
+            # --------------
+            #  Loop Checked
+            # --------------
+
+            vertices_2D = []
+            triangles_2D = []
+
+            filtered_boxes = []
+
+            for pred_idx in range(len(pred_checked)): 
+
+                pred = pred_checked[pred_idx]
+                model_id = pred[7]
+
+                with open('../../baidu_data/models/json/%s.json' % car_id2name[model_id]) as json_file:
+                    data = json.load(json_file)
+                vertices  = np.c_[np.array(data['vertices']), np.ones((len(data['vertices']), 1))].transpose()
+                triangles = np.array(data['faces'])-1
+                corners3D = get_3D_corners(vertices)
+
+                x_sol = [pred[3],pred[4],pred[5],pred[0],pred[1],pred[2]]
+                R_pr = Rotation.from_euler('xyz', [x_sol[3], x_sol[4], -x_sol[5]+np.pi]).as_dcm().T
+                Rt_pr = np.concatenate((R_pr, np.array(x_sol[:3]).reshape(-1,1)), axis=1)
+
 
                 proj_corners2D  = np.transpose(compute_projection(corners3D, Rt_pr, K))
                 proj_corners2D[:, 0] = proj_corners2D[:, 0] / 3384.0
                 proj_corners2D[:, 1] = (proj_corners2D[:, 1] - 1497.0) / (2710.0-1497.0) 
 
+                new_box = [0]*len(boxes[0])
                 for j in range(num_keypoints-2):
-                    filtered_boxes[i][9+2*j]   = proj_corners2D[j, 0]
-                    filtered_boxes[i][9+2*j+1] = proj_corners2D[j, 1]
+                    new_box[9+2*j]   = proj_corners2D[j, 0]
+                    new_box[9+2*j+1] = proj_corners2D[j, 1]
+                filtered_boxes.append(new_box)
 
                 vertices_proj_2d = np.transpose(compute_projection(vertices, Rt_pr, K))
                 vertices_proj_2d[:, 0] = vertices_proj_2d[:, 0] / 3384.0
                 vertices_proj_2d[:, 1] = (vertices_proj_2d[:, 1] - 1497.0) / (2710.0-1497.0) 
+
                 vertices_2D.append(vertices_proj_2d)
-
-                # vertices_colored =  np.c_[coords[:,:3], np.ones((len(data['vertices']), 1))].transpose()
-                # vertices_proj_2d_colored = np.transpose(compute_projection(vertices_colored, Rt_pr, K))
-                # vertices_proj_2d_colored = np.c_[vertices_proj_2d_colored, coords[:,3]]
-                # vertices_proj_2d_colored[:, 0] = vertices_proj_2d_colored[:, 0] / 3384.0
-                # vertices_proj_2d_colored[:, 1] = (vertices_proj_2d_colored[:, 1] - 1497.0) / (2710.0-1497.0) 
-                # vertices_2D_colored.append(vertices_proj_2d_colored)
-
                 triangles_2D.append(triangles)
 
-                
-        if visualize:
 
             if (img_id in flipped) and flipped[img_id]:
                 img = img.transpose(Image.FLIP_LEFT_RIGHT)
 
+            for segm_idx in range(len(segms)):
+                mask = Image.fromarray(np.uint8(segms[segm_idx]*255))
+                white_img = Image.new('RGB', img.size, (255, 255, 255))
+                img = Image.composite(white_img, img, mask)
+
+
             plot_boxes(img, filtered_boxes, '../../baidu_data/predictions/%s.jpg' % img_id, class_names, vertices_2D, triangles_2D)
-            #plot_boxes(img, boxes, '../../baidu_data/predictions/%s.jpg' % img_id, class_names, vertices_2D_colored)
+
 
         pred_str = ''
-
-        # # Filter With IOU
-        # pred_candidates = np.array(pred_candidates)
-        # preds_mask = np.ones(pred_candidates.shape[0], dtype=bool)
-        # for segm_idx in range(len(segms)):
-        #     match_idx = 0
-        #     max_iou = 0.0
-        #     for pred_idx in range(len(pred_candidates[preds_mask])):
-        #         pred = pred_candidates[preds_mask][pred_idx]
-        #         x0 = [pred[3],pred[4],pred[5],pred[0],pred[1],pred[2]]
-        #         model_id = pred[7]
-        #         with open('../../baidu_data/models/pkl/%s.pkl' % car_id2name[model_id], 'rb') as pkl_file:
-        #             coords = pickle.load(pkl_file)
-        #         coords[(coords[:,3]==0)&(coords[:,1]<-0.3),3]=7
-
-        #         iou = -neg_iou_mask(x0,coords,segms[segm_idx])
-        #         if iou > max_iou:
-        #             max_iou = iou
-        #             match_idx = pred_idx
-        #     if max_iou == 0.0:
-        #         continue
-        #     print(max_iou)
-        #     pred = pred_candidates[preds_mask][match_idx]
-        #     print(pred)
-        #     pred_str += '%f %f %f %f %f %f %f ' % (pred[0],pred[1],-pred[2]+np.pi,pred[3],pred[4],pred[5],pred[6])
-        #     preds_mask[pred_idx] = 0
-
-        # Filter With Score
-        pred_candidates = np.array(pred_candidates)
-        for pred_idx in range(len(pred_candidates)):
-            pred = pred_candidates[pred_idx]
+        pred_checked = np.array(pred_checked)
+        for pred_idx in range(len(pred_checked)):
+            pred = pred_checked[pred_idx]
             pred_str += '%f %f %f %f %f %f %f ' % (pred[0],pred[1],pred[2],pred[3],pred[4],pred[5],pred[6])
 
         # Save
@@ -362,6 +366,11 @@ def detect_skimage(cfgfile, weightfile, imgfile):
 
     class_names = load_class_names(namesfile)
     plot_boxes_cv2(img, boxes, savename='predictions.jpg', class_names=class_names)
+
+def rotate(x, angle):
+    x = x + angle
+    x = x - (x + np.pi) // (2 * np.pi) * 2 * np.pi
+    return x
 
 if __name__ == '__main__':
     if len(sys.argv) == 5:
